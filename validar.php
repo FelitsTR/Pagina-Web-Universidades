44d213bd71663731c5419ef65a14a7262cdffb5c
ErickEspinoza2077 <71611552+ErickEspinoza2077@users.noreply.github.com>
2020-11-23 22:33:45 -0500

    sis

--- validar.php
+++ validar.php
@@ -1,19 +1,17 @@
 <?php
-if(isset($_POST['Iniciar'])){
-include('conexion.php');
-$correo_usu = $_POST['correo_usu'];
-$clave_usu = $_POST['clave_usu'];
-$Query= "SELECT * FROM usuarios WHERE correo_usu='".$correo_usu."' and clave_usu='".$clave_usu."'";
-$resultado = mysqli_query($conexion, $Query);
+function fsConfigs(){
+    if(isset($_POST['Iniciar'])){
+    include('conexion.php');
+    $correo_u = $_POST['correo_u'];
+    $clave_uni = $_POST['clave_uni'];
+    $ID_U= "SELECT id_usu_uni FROM usuarios_uni WHERE correo_u='".$correo_u."' and clave_uni='".$clave_uni."'";
+    $id_base = $mysqli->query($ID_U);
+    return $id_base;
+    }
+}
+// function fsConfigs(){
 
-$nr = mysqli_num_rows($resultado);
-if($nr == 1){
-    echo "bienvenido";
-    //echo '<a href="universidades.html"></a>';
-    
-}
-else if($nr == 0){
-    echo "Este usuario no existe";
-}
-}
+//     $this->settings = $this->getConfigs();
+
+// }
 ?>
